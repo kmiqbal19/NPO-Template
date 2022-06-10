7da4636cf0a7eda88dbc3c3d85c230c3900d3965
kmiqbal19 <km.iqbal01@gmail.com>
2022-06-10 22:51:13 +0200

    implemented nav bg changing on scroll

--- src/Components/NavBar/navbar.jsx
+++ src/Components/NavBar/navbar.jsx
@@ -6,8 +6,18 @@ import { Link } from "react-router-dom";
 import logo from "../../Assets/logo.png";
 function NavBar() {
   const [toggle, setToggle] = useState(false);
+  const [navBar, setNavBar] = useState(false);
+  const changeNavBg = () => {
+    if (window.scrollY >= 100) {
+      setNavBar(true);
+    } else {
+      setNavBar(false);
+    }
+  };
+  window.addEventListener("scroll", changeNavBg);
+
   return (
-    <nav id="navbar" className="app__nav">
+    <nav id="navbar" className={navBar ? "app__nav active" : "app__nav"}>
       <div className="app__nav-logo">
         <img src={logo} alt="main-logo" />
       </div>
