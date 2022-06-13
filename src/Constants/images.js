df5233cef822038c3f0941be6728a67f217c9d75
kmiqbal19 <km.iqbal01@gmail.com>
2022-06-13 19:59:38 +0200

    updated bg sizes

--- src/Constants/images.js
+++ src/Constants/images.js
@@ -4,6 +4,7 @@ import donateBg from "../Assets/donate-bg.jpg";
 import emailLogo from "../Assets/email-logo.png";
 import whatsappLogo from "../Assets/whatsapp-logo.png";
 import bdLogo from "../Assets/bangladesh.png";
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   logo,
   bkashLogo,
